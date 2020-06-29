453c9ca5c94fdf65aacc213fe5d93caf8020a738
Ventina Limbong <47500322+yoonaim@users.noreply.github.com>
2020-06-29 12:07:52 +0700

    Update package_percabangan.java

--- package_percabangan.java
+++ package_percabangan.java
@@ -43,14 +43,8 @@ public class Percabangan {
         txt_hasil.setText(bilangan);
     }
 }
-/**
- * Compares two {@code int} values numerically.
- * The value returned is identical to what would be returned by:
- * <pre>
- *    Integer.valueOf(x).compareTo(Integer.valueOf(y))
- * </pre>
- *
- * @param  x the first {@code int} to compare
+ 
+/* @param  x the first {@code int} to compare
  * @param  y the second {@code int} to compare
  * @return the value {@code 0} if {@code x == y};
  *         a value less than {@code 0} if {@code x < y}; and
