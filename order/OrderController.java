f551636b8b64690c5c06113e41456446ef9a38fb
Octaviany <63240238+octav17@users.noreply.github.com>
2020-06-29 12:00:09 +0700

    Update OrderController.java

--- order/OrderController.java
+++ order/OrderController.java
@@ -7,6 +7,6 @@ public class ContohKondisi {
     public static void main(String[] args) {
         /* type of kondisi 
         is boolean*/
-        boolean kondisi;
-        kondisi= (1+1) == 2 && 2>1;
         // we have 2 condition here
+    }
+}
