451badee616a71b57c898f4ce37aa73225e55f62
Im Yoona <47500322+yoonaim@users.noreply.github.com>
2020-07-15 23:29:10 +0700

    Update OrderController.java

--- order/OrderController.java
+++ order/OrderController.java
@@ -3,10 +3,3 @@
 
 // create a class contohkondisi here
 public class ContohKondisi {
-
-    public static void main(String[] args) {
-        /* type of kondisi 
-        is boolean*/
-        boolean kondisi;
-        kondisi= (1+1) == 2 && 2>1;
-        // we have 2 condition here
