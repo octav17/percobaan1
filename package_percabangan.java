8f40e5bb6bb4571f1aac8091abe0e73a5117ae3c
Octaviany <63240238+octav17@users.noreply.github.com>
2020-07-15 23:28:42 +0700

    Update package_percabangan.java

--- package_percabangan.java
+++ package_percabangan.java
@@ -29,3 +29,36 @@ public class Percabangan {
             jumlah = bil1 - bil2;
             bilangan = Double.toString(jumlah);            
             break;
+        case 3:
+            bil2 = Double.parseDouble(String.valueOf(txt_hasil.getText()));
+            jumlah = bil1 * bil2;
+            bilangan = Double.toString(jumlah);            
+            break;
+        case 4:
+            bil2 = Double.parseDouble(String.valueOf(txt_hasil.getText()));
+            jumlah = bil1 / bil2;
+            bilangan = Double.toString(jumlah);            
+            break;  
+        }
+        txt_hasil.setText(bilangan);
+    }
+    private void bt_0ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // TODO add your handling code here:
+        bil +="nilai"; // nilai ganti dengan angka 0, 1, 1         
+        txt_hasil.setText(bil); // menampilkan angka kedalam jTextField
+    }
+}
+/**
+ * Compares two {@code int} values numerically.
+ * The value returned is identical to what would be returned by:
+ * <pre>
+ *    Integer.valueOf(x).compareTo(Integer.valueOf(y))
+ * </pre>
+ *
+ * @param  x the first {@code int} to compare
+ * @param  y the second {@code int} to compare
+ * @return the value {@code 0} if {@code x == y};
+ *         a value less than {@code 0} if {@code x < y}; and
+ *         a value greater than {@code 0} if {@code x > y}
+ * @since 1.7
+ */
