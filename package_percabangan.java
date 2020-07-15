add3f8d05338139b0ec2944e4002b6f4964e2c82
Im Yoona <47500322+yoonaim@users.noreply.github.com>
2020-07-15 23:24:17 +0700

    Update package_percabangan.java

--- package_percabangan.java
+++ package_percabangan.java
@@ -42,9 +42,20 @@ public class Percabangan {
         }
         txt_hasil.setText(bilangan);
     }
+    private void bt_0ActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        // TODO add your handling code here:
+        bil +="nilai"; // nilai ganti dengan angka 0, 1, 1         
+        txt_hasil.setText(bil); // menampilkan angka kedalam jTextField
+    }
 }
- 
-/* @param  x the first {@code int} to compare
+/**
+ * Compares two {@code int} values numerically.
+ * The value returned is identical to what would be returned by:
+ * <pre>
+ *    Integer.valueOf(x).compareTo(Integer.valueOf(y))
+ * </pre>
+ *
+ * @param  x the first {@code int} to compare
  * @param  y the second {@code int} to compare
  * @return the value {@code 0} if {@code x == y};
  *         a value less than {@code 0} if {@code x < y}; and
